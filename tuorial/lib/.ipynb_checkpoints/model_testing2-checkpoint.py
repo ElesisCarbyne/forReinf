06d98292cbef3369656fb998b0f2f2d0e55a3674
Carbyne06 <kranvagn9125@naver.com>
2024-12-20 08:04:35 +0900

    commit-2024/12/20

--- tuorial/lib/.ipynb_checkpoints/model_testing2-checkpoint.py
+++ tuorial/lib/.ipynb_checkpoints/model_testing2-checkpoint.py
@@ -28,26 +28,34 @@ def model(x, unpacked_params):
 
     return y
 
-def worker(pop):
-    ''' 학습 후 시험(test) '''
+def max_gene(pop):
     max_fit = 0
     max_idx = 0
     for i, x in enumerate(pop):
         if x["fitness"] > max_fit:
             max_fit = x["fitness"]
             max_idx = i
+    return max_idx
+
+def worker(pop):
+    ''' 학습 후 시험(test) '''
+    max_idx = max_gene(pop)
     model_params = unpack_params(pop[max_idx]["params"])
+    
     env = gym.make("CartPole-v1", render_mode="human") # 카트폴 환경 불러오기
+    cur_state = torch.from_numpy(env.reset()[0]).float() # 환경 초기화 및 초기 상태 반환
     
     for i in range(100):
-        cur_state = torch.from_numpy(env.reset()[0]).float() # 환경 초기화 및 초기 상태 반환
-        logits = model(cur_state, model_params)
+        with torch.no_grad():
+            logits = model(cur_state, model_params)
         action = torch.distributions.categorical.Categorical(logits=logits).sample() # 옆의 코드는 카테고리컬 분포 말고 다항 분포로도 구현할 수 있다
                                                                                      # logit에 근거한 확률분포를 바탕으로 2개의 행동 중 하나를 뽑는다
-        next_state, _, done, _, _ = env.step(action.numpy())
+        next_state, _, done, _, _ = env.step(action.item())
         if done:
             print("Lost: rechead end of game")
-            cur_state = torch.from_numpy(env.reset()[0]).float()
+            return
+        cur_state = torch.from_numpy(next_state).float()
+        
         env.render() # 에이전트가 현재 보고 있는 것의 시각화를 위해 환경을 렌더링 한다
                      # 이전에는 render() 메서드가 인자를 받았지만, 지금은 이 인자를 make() 메서드가 받도록 수정되었다
     print("Model did not failed on game!!!")
--- tuorial/lib/model_testing3.py
+++ tuorial/lib/model_testing3.py
@@ -1,32 +1,53 @@
 import torch
+from torch import nn
 import gymnasium as gym
 import numpy as np
 
-def unpack_params(params, layers=[(25, 4), (10, 25), (2, 10)]):
+class LR_block(nn.Module):
+    def __init__(self, shape):
+        super().__init__()
+        self.linear = nn.Linear(shape[0], shape[1])
+        self.relu = nn.ReLU()
+        # 유전 알고리즘의 학습은 역전파를 통한 학습과 다르므로 배치 정규화와 드롭아웃은 사용해봐야 의미가 없다
+
+    def forward(self, x):
+        output = self.relu(self.linear(x))
+
+        return output
+
+class Model(nn.Module):
+    def __init__(self, layers):
+        super().__init__()
+        self.body = nn.Sequential(*[LR_block(shape) for shape in layers[:-1]])
+        self.tail_top = nn.Linear(layers[-1][0], layers[-1][1])
+        self.tail_bottom = nn.LogSoftmax(dim=0)
+
+    def forward(self, x):
+        output = self.body(x)
+        output = self.tail_bottom(self.tail_top(output))
+
+        return output
+
+def unpack_params(params, layers):
     # 만약 어떤 은닉층이 노드가 10개이고 크기가 4인 1차원 텐서가 입력이라면
     # 이 은닉층의 가중치 텐서는 10 X 4 형태의 텐서가 된다
     # params는 모델을 이루는 모든 계층들의 학습 가능한 매개변수들을 1차원 텐서 형태로 만들어 놓은 것이다(flatten)
     unpacked_params = []
-    e = 0
-    for i, l in enumerate(layers):
-        s, e = e, e + np.prod(l) # np.prod()는 주어진 축(axis) 상의 배열 원소들에 대한 곱을 수행한다
-        weights = params[s:e].view(l) # 첫 번째 은닉층부터 시작하여 각 계층의 가중치 텐서를 params로부터 추출한다
-        s, e = e, e+l[0]
-        bias = params[s:e] # 첫 번째 은닉층부터 시작하여 각 계층의 편향 텐서를 params로부터 추출한다
-        unpacked_params.extend([weights, bias])
+    start = 0
+    for l in layers:
+        division = np.prod(l)
+        stop = start + division + l[1]
+        layer_vec = params[start:stop] # 각 계층의 매개변수 벡터를 추출한다
+        unpacked_params.extend([layer_vec[0:division].reshape((l[1], l[0])), layer_vec[division:]]) # 각 계층에 해당하는 매개변수 벡터로부터 다시 가중치 벡터와 편향 벡터를 추출한다
+        start = stop
 
     return unpacked_params
 
-def model(x, unpacked_params):
-    l1, b1, l2, b2, l3, b3 = unpacked_params
-    y = torch.nn.functional.linear(x, l1, b1) # torch.nn.functional.linear()에는 입력과 가중치 텐서, 편향 텐서가 인자로 전달된다
-    y = torch.relu(y)
-    y = torch.nn.functional.linear(y, l2, b2)
-    y = torch.relu(y)
-    y = torch.nn.functional.linear(y, l3, b3)
-    y = torch.log_softmax(y, dim=0)
+def construct_model(state_dict, parameters_set):
+    for i, key in enumerate(state_dict):
+        state_dict[key].copy_(parameters_set[i]) # 모델의 매개변수 텐서를 개체의 매개변수 텐서로 모두 덮어씌운다
 
-    return y
+    return state_dict
 
 def max_gene(pop):
     max_fit = 0
@@ -35,19 +56,22 @@ def max_gene(pop):
         if x["fitness"] > max_fit:
             max_fit = x["fitness"]
             max_idx = i
+
+    print(max_fit)
     return max_idx
 
-def worker(pop):
+def worker(pop, layers):
     ''' 학습 후 시험(test) '''
     max_idx = max_gene(pop)
-    model_params = unpack_params(pop[max_idx]["params"])
+    model = Model(layers)
+    model.load_state_dict(construct_model(model.state_dict(), unpack_params(pop[max_idx]["params"], layers))) # 개체를 토대로 모델을 구성한다
     
     env = gym.make("CartPole-v1", render_mode="human") # 카트폴 환경 불러오기
     cur_state = torch.from_numpy(env.reset()[0]).float() # 환경 초기화 및 초기 상태 반환
     
     for i in range(100):
         with torch.no_grad():
-            logits = model(cur_state, model_params)
+            logits = model(cur_state)
         action = torch.distributions.categorical.Categorical(logits=logits).sample() # 옆의 코드는 카테고리컬 분포 말고 다항 분포로도 구현할 수 있다
                                                                                      # logit에 근거한 확률분포를 바탕으로 2개의 행동 중 하나를 뽑는다
         next_state, _, done, _, _ = env.step(action.item())
